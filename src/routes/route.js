91389c2827e60e30ce8500ff2c2e09af21c0c63b
pradeep <pradeeppatil8379@gmail.com>
2022-08-08 14:00:51 +0530

    lodash updates

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+const lodash= require('lodash');
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
+    const months =['jan','feb','march','april','may','june','july','august','sep','oct','nov','dec']
+    const months3= lodash.chunk(months,3);
+    console.log(months3);
+    const odd=[1,2,5,7,9,11,13,15,17,19];
+    const last9=lodash.tail(odd);
+    console.log(last9);
+    const common=[[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7]];
+    const unique =lodash.union([1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7]);
+    console.log(unique);
+    const arr =[[ 'horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]
+    const pairs=lodash.fromPairs(arr);
+    console.log(pairs);
+
     res.send('My second ever api!')
 });
 
