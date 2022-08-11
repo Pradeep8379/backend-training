b849361a7a8a9c6cbe6a1837a514bb379a55d697
pradeep <pradeeppatil8379@gmail.com>
2022-08-11 20:41:36 +0530

    voting age assignmnet

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+let persons = [
+    {
+        name: "pk",
+        age: 10,
+        votingage: false
+    },
+    {
+        name: "sk",
+        age: 20,
+        votingage: false
+    },
+    {
+        name: "aa",
+        age: 70,
+        votingage: false
+    },
+    {
+        name: "sc",
+        age: 5,
+        votingage: false
+    },
+    {
+        name: "ho",
+        age: 40,
+        votingage: false
+    }
+]
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
-
-
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data',function(req, res){
-
+router.get('/test-me/:age',function(req,res){
+    let legalage=req.params.age;
+    let arr=[];
+    for(let i=0;i<persons.length;i++){
+        let data=persons[i];
+         
+        if(data.age>legalage){
+            data['votingage']=true;
+            arr.push(data)
+        }
+        else{
+            data['votingage']=false;
+        }
+    }
+    res.send(arr)
 })
 module.exports = router;
 // adding this comment for no reason
